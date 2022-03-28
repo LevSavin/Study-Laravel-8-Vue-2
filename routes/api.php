524a077414f733155fde3291f89c71c8ba4c47c6
Anton Maslinskiy <anton.maslinsky@gmail.com>
2022-03-28 17:57:49 +0300

    Добавлены таблицы: timetables(расписание), workers(работники), services(услуги). Созданы модели и контроллеры Worker WorkerController, Service ServiceController, Timetable TimetableController Добавлены роуты в api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\TimetableController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\WorkerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\WaybillController;
@@ -18,6 +22,10 @@ use App\Http\Controllers\Api\WaybillController;
 Route::get('/waybills/create', [WaybillController::class, 'create']);
 Route::post('/waybills', [WaybillController::class, 'store']);
 Route::get('/waybills/{number}', [WaybillController::class, 'show']);
+Route::resource('/timetable', TimetableController::class);
+Route::resource('/services', ServiceController::class);
+Route::resource('/users', UserController::class);
+Route::resource('/workers', WorkerController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
