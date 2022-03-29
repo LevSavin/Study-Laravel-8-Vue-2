8e1f29010b15f3c8822147d0685ecc5375b27cf3
seydametova <seidametovaf1@gmail.com>
2022-03-29 18:04:59 +0200

    добавила календарь

--- resources/js/Router/routes.js
+++ resources/js/Router/routes.js
@@ -10,6 +10,16 @@ const routes = [
         name: '404'
     },
     {
+        path: '/scheduler',
+        component: require('../Pages/Scheduler').default,
+        name: 'scheduler'
+    },
+    {
+        path: '/scheduler/:date',
+        component: require('../Pages/ClientList').default,
+        name: 'schedulerDate'
+    },
+    {
         path: '*',
         component: require('../Pages/NotFound').default,
         name: 'not-found'
